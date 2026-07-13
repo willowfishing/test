dd7ea5b151d7fede0f2b4a565d3c0039b9808f71
RMDB <rmdb@test.com>
2026-07-13 12:18:54 +0800

    exp9 SI: restore capture_snapshot at begin + write-write conflict detection in Delete/Update executors + check_write_write_conflict in txn_mgr

--- src/execution/execution_manager.cpp
+++ src/execution/execution_manager.cpp
@@ -106,6 +106,8 @@ void QlManager::run_cmd_utility(std::shared_ptr<Plan> plan, txn_id_t *txn_id, Co
                 // 显示开启一个事务
                 context->txn_->set_txn_mode(true);
                 txn_mgr_->capture_snapshot(context->txn_);
+                txn_mgr_->capture_snapshot(context->txn_);
+                txn_mgr_->capture_snapshot(context->txn_);
                 break;
             }  
             case T_Transaction_commit:
