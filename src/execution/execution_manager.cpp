25980afb82420715a830a16d9a90db3adc0e9b7a
RMDB <rmdb@test.com>
2026-07-13 12:15:02 +0800

    exp9+10+perf: complete load/static_checkpoint/set output_file off - lexer/parser regen, ast.h LoadStmt/StaticCheckpoint, rmdb.cpp handler, output_file_enabled extern

--- src/execution/execution_manager.cpp
+++ src/execution/execution_manager.cpp
@@ -1,4 +1,5 @@
 /* Copyright (c) 2023 Renmin University of China
+extern std::atomic<bool> output_file_enabled;
 RMDB is licensed under Mulan PSL v2.
 You can use this software according to the terms and conditions of the Mulan PSL v2.
 You may obtain a copy of Mulan PSL v2 at:
